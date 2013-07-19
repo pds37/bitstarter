a8ce8fea8cdc0c3afbbe44bf1a8ad182a8c03756
pds <blackxub@naver.com>
2013-07-19 09:37:48 +0000

    commander.. bugs exist

--- grader.js
+++ grader.js
@@ -21,11 +21,27 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
-var fs = require('fs');
-var program = require('commander');
-var cheerio = require('cheerio');
+var fs = require('fs'),
+    sys = require('util'),
+    program = require('commander'),
+    cheerio = require('cheerio'),
+    rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT= "http://pacific-tor-2805.herokuapp.com/";
+
+var UrlExists = function(url) {
+    var urlstr = url.toString();
+    rest.get(urlstr).on('complete', function(result) {
+      if (result instanceof Error) {
+        sys.puts('Error: ' + result.message);
+        process.exit(1); 
+      } else {
+        return urlstr;
+      }
+    });
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -63,12 +79,17 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to url', clone(UrlExists),URL_DEFAULT)
         .parse(process.argv);
+
+sys.puts(program.file);
+sys.puts(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
