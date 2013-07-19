8f99da25074a63456a1d6abfaffb6c1e27c9bfef
pds <blackxub@naver.com>
2013-07-19 09:40:35 +0000

    commander HW. bugs exist. log delete

--- grader.js
+++ grader.js
@@ -83,9 +83,7 @@ if(require.main == module) {
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-u, --url <url>', 'Path to url', clone(UrlExists),URL_DEFAULT)
         .parse(process.argv);
-
-sys.puts(program.file);
-sys.puts(process.argv);
+Ã
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
